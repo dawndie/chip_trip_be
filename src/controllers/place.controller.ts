03aa5eb162dc36e3fd84935fd9ebe5f7f919b20a
bang.voluong <voluongbang20@gmail.com>
2022-04-07 17:41:49 +0700

    upadate

--- src/controllers/place.controller.ts
+++ src/controllers/place.controller.ts
@@ -6,6 +6,8 @@ import { Amenity } from "@/entities/amenity.entity";
 import { getCustomRepository } from "typeorm";
 import { FindOneOptions } from "typeorm";
 import { APPROVED_STATUS } from "@/const/common.const";
+import fs from "fs";
+import AWS from "aws-sdk";
 class _PlaceController extends BaseController {
   async getPlaceInformation(
     req: express.Request,
@@ -46,6 +48,23 @@ class _PlaceController extends BaseController {
     res: express.Response,
     next: express.NextFunction,
   ) {
+    const s3 = new AWS.S3({
+      accessKeyId: process.env.S3_ACCESS_KEY_ID,
+      secretAccessKey: process.env.S3_SECRET_KEY,
+    });
+
+    const fileName = "D:/Chip_trip/chip_trip_be/src/controllers/logo.png";
+    const readStream = fs.readFileSync(fileName);
+    const params = {
+      Bucket: process.env.S3_BUCKET || "photostore.voluongbang", // pass your bucket name
+      Key: "photo" + "/" + "fileName.png", // file will be saved as testBucket/contacts.csv
+      Body: readStream,
+    };
+    s3.upload(params, (s3Err: any, data: any) => {
+      if (s3Err) this.getManagedError(s3Err);
+      console.log(`File uploaded successfully at ${data}`);
+    });
+
     const amenities = req.body.amenities.map((element: number) => {
       const amenity = new Amenity();
       amenity.id = element;
